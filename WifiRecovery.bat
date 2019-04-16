a44e1c4fa348b49a605703374a3edc1991c52dbd
Ashkan Rafiee <35492414+AshkanRafiee@users.noreply.github.com>
2019-04-16 13:26:01 +0430

    Add files via upload

---
+++ WifiRecovery.bat
@@ -0,0 +1,23 @@
+@echo off
+color 02
+:::Choice
+::cls
+::set /P q=Enter The Appropriate Hotkey(Connected WifiList="l",Recovery="r"):
+::if /I "%q%" EQU "l" goto :wifilist
+::if /I "%q%" EQU "r" goto :recovery
+::if /I "%q%" EQU "e" goto :exit
+::goto :Choice
+
+:wifilist
+set "SSID="
+cls
+echo .........................................................................
+netsh wlan show profile | findstr "All User Profiles"
+goto :recovery
+
+:recovery
+echo .........................................................................
+set /P SSID=Enter Desired Wifi Name to Recover Password(Case Sensitive!): 
+netsh wlan show profile "%SSID%" key=clear | findstr "Key Content not found"
+pause
+goto :wifilist
